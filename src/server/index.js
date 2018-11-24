d3c41e57d30c06b1231606fe87df4afa046d9b7b
Spyros Desyllas <spyros2283@gmail.com>
2018-11-24 20:38:26 +0000

    nodejs express configuratioin

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(publicweb));
 console.log(`serving ${publicweb}`);
 app.get('*', (req, res) => {
-  res.sendFile(`index.html`, { root: publicweb });
+  res.sendFile(`../index.html`, { root: publicweb });
 });
 
 const port = process.env.SERVER_PORT || '3000';
